54e09742387034c185d8748e497a6fb67ae3f252
Jacob Leiken <jacobleiken@gmail.com>
2017-04-13 16:19:52 -0400

    Added username picker

--- src/main/resources/practice-html/drop_advanced.js
+++ src/main/resources/practice-html/drop_advanced.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+	let isMaking;
 	"use strict";
 	$("#advanced_ops").hide();
-	$("#advanced").on('click', function() {
+	$("#advanced").on('click', () => {
 		if ($("#advanced_ops").is(":hidden")) {
 			$("#advanced_ops").show();
 			$("#advanced_im").attr("src", "down-triangle.png");
@@ -9,5 +10,27 @@ $(document).ready(function() {
 			$("#advanced_ops").hide();
 			$("#advanced_im").attr("src", "side-triangle.png");
 		}
+	});
+	
+	$("#start_game").on('click', () => {
+		if ($("#game_name").val() === "") {
+			$("#game_name").animate({color:'red'}, 400).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+			$("#game_name").animate({color:'white'}, 100);
+			return;
+		}
+		isMaking = true;
+		$("#main").hide();
+		$("#uname_picker").show();
+	});
+	
+	$("#open_games").on('click', () => {
+		isMaking = false;
+		$("#main").hide();
+		$("#uname_picker").show();
+	});
+	
+	$("#back_main").on('click', () => {
+		$("#uname_picker").hide();
+		$("#main").show();
 	});
 });
